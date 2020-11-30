fa1a57cca4c86e75ec03ce59cdbdfb0fe014d22d
zeidfazazi <vincedzeid@gmail.com>
2020-11-30 10:19:08 +0100

    Adaptation aux nouveaux icones

--- rating.js
+++ rating.js
@@ -2,20 +2,22 @@
 	for (i=1;i<6;i++) {
 		var img			=document.getElementById('vote'+i);
 		img.onmouseover	=function() {Noter(this.id);};
-		img.onmouseout	=function() {Denoter(this.id);};
+		img.onmouseout	=function() {Denoter(this.id);}
 	}
 })()
 
 function Noter(Star) {
 	StarNb=Star.slice(4);
 	for (i=1;i<(StarNb*1)+1;i++) {
-		document.getElementById('vote'+i).src='icons/etoile pleine.png';
+		document.getElementById('vote'+i).classList.remove("far");
+		document.getElementById('vote'+i).classList.add("fas");
 	}
 }
 
 function Denoter(Star) {
 	StarNb=Star.slice(4);
 	for (i=1;i<(StarNb*1)+1;i++) {
-		document.getElementById('vote'+i).src='icons/etoile vides.png';
+		document.getElementById('vote'+i).classList.remove("fas");
+		document.getElementById('vote'+i).classList.add("far");
 	}
 }
