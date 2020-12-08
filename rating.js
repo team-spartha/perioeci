c49ad7e1e69fac9feaff130499242c3ec9daaa5d
Speykious <speykious@gmail.com>
2020-12-08 19:59:40 +0100

    Wowwowwow! Consistency guys!

--- rating.js
+++ rating.js
@@ -1,23 +1,23 @@
 (function notation() {
-	for (i=1;i<6;i++) {
-		var img			=document.getElementById('vote'+i);
-		img.onmouseover	=function() {Noter(this.id);};
-		img.onmouseout	=function() {Denoter(this.id);}
-	}
+  for (i = 1; i < 6; i++) {
+    var img = document.getElementById('vote' + i);
+    img.onmouseover = function() { note(this.id); };
+    img.onmouseout = function() { denote(this.id); }
+  }
 })()
 
-function Noter(Star) {
-	StarNb=Star.slice(4);
-	for (i=1;i<(StarNb*1)+1;i++) {
-		document.getElementById('vote'+i).classList.remove("far");
-		document.getElementById('vote'+i).classList.add("fas");
-	}
+function note(star) {
+  StarNb = star.slice(4);
+  for (i = 1; i < (StarNb * 1) + 1; i++) {
+    document.getElementById('vote' + i).classList.remove("far");
+    document.getElementById('vote' + i).classList.add("fas");
+  }
 }
 
-function Denoter(Star) {
-	StarNb=Star.slice(4);
-	for (i=1;i<(StarNb*1)+1;i++) {
-		document.getElementById('vote'+i).classList.remove("fas");
-		document.getElementById('vote'+i).classList.add("far");
-	}
+function denote(star) {
+  StarNb = star.slice(4);
+  for (i = 1; i < (StarNb * 1) + 1; i++) {
+    document.getElementById('vote' + i).classList.remove("fas");
+    document.getElementById('vote' + i).classList.add("far");
+  }
 }
--- slider.js
+++ slider.js
@@ -1,54 +1,49 @@
-var slide = document.querySelectorAll('.shot');
-var nbImg = slide.length;
-var items = document.querySelectorAll('.software-screen.overview img');
-var precedent = document.querySelector('.software-screen-btn-left');
-var count = 0;
-var suivant = document.querySelector('.software-screen-btn-right');
+const slide = document.querySelectorAll('.shot');
+const nbImg = slide.length;
+const items = document.querySelectorAll('.software-screen.overview img');
+const prev = document.querySelector('.software-screen-btn-left');
+const next = document.querySelector('.software-screen-btn-right');
+let count = 0;
 
-
-function SlideSuivante() {
-   slide[count].classList.remove('active');
-   items[count].classList.remove('active');
-   if(count < nbImg-1){
-       count++;
-   }
-   else{
-       count=0;
-   }
-   console.log(count)
-   slide[count].classList.add('active');
-   items[count].classList.add('active');
+function nextSlide() {
+  slide[count].classList.remove('active');
+  items[count].classList.remove('active');
+  if (count < nbImg - 1)
+    count++;
+  else
+    count = 0;
+  console.log(count)
+  slide[count].classList.add('active');
+  items[count].classList.add('active');
 }
-function SlidePrécédente() {
-   slide[count].classList.remove('active');
-   items[count].classList.remove('active');
-   if(count>0){
-       count--;
-   }
-   else{
-       count=nbImg-1;
-   }
-   console.log(count);
-   slide[count].classList.add('active');
-   items[count].classList.add('active');
+
+function prevSlide() {
+  slide[count].classList.remove('active');
+  items[count].classList.remove('active');
+  if (count > 0)
+    count--;
+  else
+    count = nbImg - 1;
+  console.log(count);
+  slide[count].classList.add('active');
+  items[count].classList.add('active');
 }
 
 (function transition() {
-    for(i = 1;i<5;i++){
-        var screen = document.getElementById("screen"+i);
-        screen.onclick = function(){Activation(this.id);};
-    }
+  for (i = 1; i < 5; i++) {
+    const screen = document.getElementById("screen" + i);
+    screen.onclick = function() { activation(this.id); };
+  }
 })()
 
-function Activation(screen){
-    var index = screen.slice(6);
-    slide[count].classList.remove('active');
-    items[count].classList.remove('active');
-    count = index-1;
-    slide[count].classList.add('active');
-    items[count].classList.add('active');
+function activation(screen) {
+  const index = screen.slice(6);
+  slide[count].classList.remove('active');
+  items[count].classList.remove('active');
+  count = index - 1;
+  slide[count].classList.add('active');
+  items[count].classList.add('active');
 }
 
-
-suivant.addEventListener('click',SlideSuivante);
-precedent.addEventListener('click',SlidePrécédente);
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
