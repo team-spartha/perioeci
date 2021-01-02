c156dab8f4ca001f58edc85c3d0aad1db01d43b4
Fazazi Zeid <vincedzeid@gmail.com>
2021-01-02 14:13:00 +0100

    Add code to manage the likes in comment section

---
+++ client/script/likes.js
@@ -0,0 +1,21 @@
+const like_btn = document.querySelector(".comment-user.options button:first-child")
+const like_btn_icon = document.querySelector(".comment-user.options button:first-child i")
+const like_btn_label = document.querySelector(".comment-user.options button:first-child h4")
+const cpt = document.querySelector(".likes h4")
+
+like_btn.addEventListener("click", () =>{
+    if(like_btn_icon.classList.contains("far")){
+        const new_cpt = parseInt(cpt.innerHTML,10) + 1
+        cpt.innerHTML = new_cpt
+        like_btn_icon.classList.remove("far")
+        like_btn_icon.classList.add("fas")
+        like_btn_label.innerHTML = "Aimé"
+    }
+    else{
+        const new_cpt = parseInt(cpt.innerHTML,10) - 1
+        cpt.innerHTML = new_cpt
+        like_btn_icon.classList.remove("fas")
+        like_btn_icon.classList.add("far")
+        like_btn_label.innerHTML = "J'aime"
+    }
+})
