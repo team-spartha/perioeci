61bb169868b6734430ca85b5e9af1ea0f4efb94f
Speykious <speykious@gmail.com>
2021-01-04 15:52:41 +0100

    Synced database connection

--- server/db/init.js
+++ server/db/init.js
@@ -6,4 +6,8 @@ const generateModel = ({ name, table }) => (connection) => connection.define(nam
 const uri = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@localhost:5432/perioeci`;
 const connection = new Sequelize(uri);
 
+connection.sync()
+  .then(() => "Database connection established")
+  .catch(e => `\x1b[31m\x1b[1mERROR:\x1b[0m ${e}`);
+
 generateModel(modelUser)(connection);
