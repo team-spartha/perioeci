d28eb6f698ae53c6979646e7cf2c6cad48cb00a3
Speykious <speykious@gmail.com>
2021-01-04 14:22:54 +0100

    Changed the server's express static directory due to frontend reorganization

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 // The path is relative to the root directory of the project
-const clientDir = "client/";
 const port = 6942;
 
 const app = express();
 app.listen(port, () => console.log("Listening on port", port, "(·_· )"));
-app.use(express.static(clientDir));
+app.use(express.static("client/pages/"));
 app.use(express.json({ limit: "1mb" }));
