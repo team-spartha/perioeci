e06481cb73a3207a50cccd496cec54b80711b5f4
Speykious <speykious@gmail.com>
2021-01-11 17:55:20 +0100

    Trying something

--- server/index.js
+++ server/index.js
@@ -7,10 +7,18 @@ const port = 6942;
 const app = express();
 app.listen(port, () => console.log("Listening on port", port, "(·_· )"));
 // The path is relative to the root directory of the project
-//app.use(express.static("client/"));
+app.use(express.static("client/"));
 app.use(express.json({ limit: "1mb" }));
 
 app.set("view engine", "ejs");
+app.set("views", "views");
 app.get("/", (_req, res) => {
-  res.render("index", { petitTest: "Petit test" });
+  res.render("index", {
+    page_title: "Perioeci Market",
+    icon_user: "resources/icons/account.png"
+  });
+})
+
+app.get("/header", (_req, res) => {
+  res.render("header", { icon_user: "../resources/icons/account.png" });
 })
