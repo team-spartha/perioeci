1189e30075264e15c4149a8c928bf78ebed7c7dd
Speykious <speykious@gmail.com>
2021-01-12 19:57:00 +0100

    I guess the comment was useless

--- server/index.js
+++ server/index.js
@@ -6,15 +6,13 @@ const port = 6942;
 
 const app = express();
 app.listen(port, () => console.log("Listening on port", port, "(·_· )"));
-// The path is relative to the root directory of the project
-const route = (name) => require(`./routes/${name}`);
-app.use("/news", route("news"));
+
 app.use(express.static("client/"));
 app.use(express.json({ limit: "1mb" }));
-
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.get("/", (_req, res) => res.render("index"));
 
-
+const route = (name) => require(`./routes/${name}`);
+app.use("/news", route("news"));
