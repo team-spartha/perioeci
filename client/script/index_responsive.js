6ca629aeee153873b53682ec4575fa1dbf9b2b43
Fazazi Zeid <vincedzeid@gmail.com>
2021-01-17 00:48:45 +0100

    Add code to improve responsivity

--- client/script/index_responsive.js
+++ client/script/index_responsive.js
@@ -4,33 +4,63 @@ const btn_menu = document.querySelector(".fas.fa-bars");
 var theme_bar = document.querySelector(".theme-bar");
 var OS_bar = document.querySelector(".OS-bar");
 
-var opened = false;
+let opened_search_bar = false;
+let opened_OS_theme_bars = true;
+let mobile_setup = false;
+
 if(screen.width<1000){
-    OS_bar.style.display = "none"
-}
-
-function transition() {
-    if(search_bar.classList.contains('active')){
-        search_bar.classList.remove('active');
-    }
-    else{
-        search_bar.classList.add('active');
-    }
-}
-
-function transition_menu(){
-    if(opened){
-        theme_bar.classList.remove("active")
+    if(!mobile_setup){
         OS_bar.style.display = "none"
-        opened = false
-        search_bar.classList.remove("boxy")
+        theme_bar.style.display = "none"
+        opened_OS_theme_bars = false;
+        search_bar.style.display = "none";
+        opened_search_bar = false;
+    }
+    mobile_setup = true;
+}
+
+window.addEventListener('resize',()=>{
+    if(screen.width<1000){
+        if(!mobile_setup){
+            OS_bar.style.display = "none"
+            theme_bar.style.display = "none"
+            opened_OS_theme_bars = false;
+            search_bar.style.display = "none";
+            opened_search_bar = false;
+        }
+        mobile_setup = true;
     }
     else{
-        theme_bar.classList.add("active")
+        mobile_setup = false;
         OS_bar.style.display = "flex"
-        opened = true
-        search_bar.classList.add("boxy")
+        theme_bar.style.display = "flex"
+        opened_OS_theme_bars = true;
     }
-}
-btn.addEventListener('click',transition);
-btn_menu.addEventListener('click',transition_menu);
+
+})
+
+btn.addEventListener('click',() => {
+    if(opened_search_bar){
+        search_bar.style.display = "none";
+        opened_search_bar = false;
+    }
+    else{
+        search_bar.style.display = "flex";
+        opened_search_bar = true;
+    }
+});
+
+btn_menu.addEventListener('click',() => {
+    if(opened_OS_theme_bars){
+        OS_bar.style.display = "none"
+        theme_bar.style.display = "none"
+        search_bar.classList.remove("boxy");
+        opened_OS_theme_bars = false;
+    }
+    else{
+        OS_bar.style.display = "flex"
+        theme_bar.style.display = "flex"
+        search_bar.classList.add("boxy");
+        opened_OS_theme_bars = true;
+    }
+});
