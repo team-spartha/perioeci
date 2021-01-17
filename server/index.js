7518fa9c929282d53165f97bdc6d8d256de04b4d
Speykious <speykious@gmail.com>
2021-01-17 02:49:21 +0100

    Modularity STONKS

--- server/index.js
+++ server/index.js
@@ -1,6 +1,14 @@
 require("dotenv-flow").config();
 require("./db/init");
 
+const YAML = require("yaml");
+const { readdirSync, readFileSync } = require("fs");
+
+const softwares = readdirSync(`${__dirname}/software-pages/`)
+  .map(pagename => YAML.parse(
+    readFileSync(`${__dirname}/software-pages/${pagename}`, "utf8")
+  ));
+
 const express = require("express");
 const port = 6942;
 
@@ -12,9 +20,9 @@ app.use(express.json({ limit: "1mb" }));
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-app.get("/", (_req, res) => res.render("index"));
+app.get("/", (_req, res) => res.render("index", { softwares }));
 
-const route = (name) => require(`./routes/${name}`);
+const route = name => require(`./routes/${name}`);
 app.use("/news", route("news"));
 
 app.get("/:anything", (_req, res) => {
