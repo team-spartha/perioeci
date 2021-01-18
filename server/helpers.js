7df46f12e79351baca8ea01939cac0a625c9bb59
Speykious <speykious@gmail.com>
2021-01-19 00:32:48 +0100

    Added more helpers, I'll need them

--- server/helpers.js
+++ server/helpers.js
@@ -1,6 +1,14 @@
 const sortObj = (obj, predicate) =>
   Object.fromEntries(Object.entries(obj).sort(predicate));
 
+const filterObj = (obj, predicate) =>
+  Object.fromEntries(Object.entries(obj).filter(predicate));
+
+const sliceObj = (obj, ...args) =>
+  Object.fromEntries(Object.entries(obj).slice(...args));
+
 module.exports = {
-  sortObj
+  sortObj,
+  filterObj,
+  sliceObj
 }
